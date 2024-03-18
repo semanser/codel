b03fa8375b324e1d3487a8b6a87d38d08a29c3bd
Andriy Semenets <semanser@gmail.com>
2024-03-18 14:01:08 +0100

    Add a basic flow/task DB schema

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/semanser/ai-coder
 
 go 1.22.0
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible
+	gorm.io/driver/postgres v1.5.7
+	gorm.io/gorm v1.25.8
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,49 @@
 package main
 
-func main() {
-  println("Hello, World!")
+import (
+  "github.com/jackc/pgx/pgtype"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Flow struct {
+  gorm.Model
+  ID uint
 }
 
+type TaskType string
+
+const (
+  Input TaskType = "input"
+  Action TaskType = "action"
+)
+
+type TaskStatus = string
+
+const (
+  InProgress TaskStatus = "in_progress"
+  Finished TaskStatus = "finished"
+  Stoped TaskStatus = "stoped"
+  Failed TaskStatus = "failed"
+)
+
+type Task struct {
+  gorm.Model
+  ID uint
+  Type TaskType
+  Status TaskStatus
+  Args pgtype.JSONB
+  Results pgtype.JSONB
+}
+
+func main() {
+  dsn := "postgresql://postgres@localhost/ai-coder"
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+  if err != nil {
+    panic("failed to connect database")
+  }
+
+  // Migrate the schema
+  db.AutoMigrate(&Flow{}, &Task{})
+
+}
