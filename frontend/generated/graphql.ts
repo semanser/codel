1a40840d122bd70ba17cee09f6415c969b915d91
Andriy Semenets <semanser@gmail.com>
2024-03-19 14:25:11 +0100

    Add graphql configuration on the FE

---
+++ frontend/generated/graphql.ts
@@ -0,0 +1,362 @@
+import { IntrospectionQuery } from "graphql";
+import gql from "graphql-tag";
+import * as Urql from "urql";
+
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
+    };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  JSON: { input: any; output: any };
+  Uint: { input: any; output: any };
+};
+
+export type Flow = {
+  __typename?: "Flow";
+  id: Scalars["Uint"]["output"];
+  tasks: Array<Task>;
+};
+
+export type Mutation = {
+  __typename?: "Mutation";
+  createFlow: Flow;
+  createTask: Task;
+  stopTask: Task;
+};
+
+export type MutationCreateTaskArgs = {
+  id: Scalars["Uint"]["input"];
+  query: Scalars["String"]["input"];
+};
+
+export type MutationStopTaskArgs = {
+  id: Scalars["Uint"]["input"];
+};
+
+export type Query = {
+  __typename?: "Query";
+  flows: Array<Flow>;
+};
+
+export type Subscription = {
+  __typename?: "Subscription";
+  taskAdded: Task;
+  taskUpdated: Task;
+};
+
+export type Task = {
+  __typename?: "Task";
+  args: Scalars["JSON"]["output"];
+  id: Scalars["Uint"]["output"];
+  results: Scalars["JSON"]["output"];
+  status: TaskStatus;
+  type: TaskType;
+};
+
+export enum TaskStatus {
+  Failed = "failed",
+  Finished = "finished",
+  InProgress = "inProgress",
+  Stopped = "stopped",
+}
+
+export enum TaskType {
+  Action = "action",
+  Input = "input",
+}
+
+export const FlowsDocument = gql`
+  query flows {
+    flows {
+      id
+    }
+  }
+`;
+
+export function useFlowsQuery(
+  options?: Omit<Urql.UseQueryArgs<FlowsQueryVariables>, "query">,
+) {
+  return Urql.useQuery<FlowsQuery, FlowsQueryVariables>({
+    query: FlowsDocument,
+    ...options,
+  });
+}
+export type FlowsQueryVariables = Exact<{ [key: string]: never }>;
+
+export type FlowsQuery = {
+  __typename?: "Query";
+  flows: Array<{ __typename?: "Flow"; id: any }>;
+};
+
+export default {
+  __schema: {
+    queryType: {
+      name: "Query",
+    },
+    mutationType: {
+      name: "Mutation",
+    },
+    subscriptionType: {
+      name: "Subscription",
+    },
+    types: [
+      {
+        kind: "OBJECT",
+        name: "Flow",
+        fields: [
+          {
+            name: "id",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "SCALAR",
+                name: "Any",
+              },
+            },
+            args: [],
+          },
+          {
+            name: "tasks",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "LIST",
+                ofType: {
+                  kind: "NON_NULL",
+                  ofType: {
+                    kind: "OBJECT",
+                    name: "Task",
+                    ofType: null,
+                  },
+                },
+              },
+            },
+            args: [],
+          },
+        ],
+        interfaces: [],
+      },
+      {
+        kind: "OBJECT",
+        name: "Mutation",
+        fields: [
+          {
+            name: "createFlow",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "OBJECT",
+                name: "Flow",
+                ofType: null,
+              },
+            },
+            args: [],
+          },
+          {
+            name: "createTask",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "OBJECT",
+                name: "Task",
+                ofType: null,
+              },
+            },
+            args: [
+              {
+                name: "id",
+                type: {
+                  kind: "NON_NULL",
+                  ofType: {
+                    kind: "SCALAR",
+                    name: "Any",
+                  },
+                },
+              },
+              {
+                name: "query",
+                type: {
+                  kind: "NON_NULL",
+                  ofType: {
+                    kind: "SCALAR",
+                    name: "Any",
+                  },
+                },
+              },
+            ],
+          },
+          {
+            name: "stopTask",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "OBJECT",
+                name: "Task",
+                ofType: null,
+              },
+            },
+            args: [
+              {
+                name: "id",
+                type: {
+                  kind: "NON_NULL",
+                  ofType: {
+                    kind: "SCALAR",
+                    name: "Any",
+                  },
+                },
+              },
+            ],
+          },
+        ],
+        interfaces: [],
+      },
+      {
+        kind: "OBJECT",
+        name: "Query",
+        fields: [
+          {
+            name: "flows",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "LIST",
+                ofType: {
+                  kind: "NON_NULL",
+                  ofType: {
+                    kind: "OBJECT",
+                    name: "Flow",
+                    ofType: null,
+                  },
+                },
+              },
+            },
+            args: [],
+          },
+        ],
+        interfaces: [],
+      },
+      {
+        kind: "OBJECT",
+        name: "Subscription",
+        fields: [
+          {
+            name: "taskAdded",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "OBJECT",
+                name: "Task",
+                ofType: null,
+              },
+            },
+            args: [],
+          },
+          {
+            name: "taskUpdated",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "OBJECT",
+                name: "Task",
+                ofType: null,
+              },
+            },
+            args: [],
+          },
+        ],
+        interfaces: [],
+      },
+      {
+        kind: "OBJECT",
+        name: "Task",
+        fields: [
+          {
+            name: "args",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "SCALAR",
+                name: "Any",
+              },
+            },
+            args: [],
+          },
+          {
+            name: "id",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "SCALAR",
+                name: "Any",
+              },
+            },
+            args: [],
+          },
+          {
+            name: "results",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "SCALAR",
+                name: "Any",
+              },
+            },
+            args: [],
+          },
+          {
+            name: "status",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "SCALAR",
+                name: "Any",
+              },
+            },
+            args: [],
+          },
+          {
+            name: "type",
+            type: {
+              kind: "NON_NULL",
+              ofType: {
+                kind: "SCALAR",
+                name: "Any",
+              },
+            },
+            args: [],
+          },
+        ],
+        interfaces: [],
+      },
+      {
+        kind: "SCALAR",
+        name: "Any",
+      },
+    ],
+    directives: [],
+  },
+} as unknown as IntrospectionQuery;
---
+++ frontend/src/graphql.ts
@@ -0,0 +1,46 @@
+import { devtoolsExchange } from "@urql/devtools";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import { createClient as createWSClient } from "graphql-ws";
+import { createClient, fetchExchange, subscriptionExchange } from "urql";
+
+import schema from "../generated/graphql.schema.json";
+
+export const cache = cacheExchange({
+  schema: schema,
+  updates: {
+    Mutation: {},
+  },
+  keys: {},
+});
+
+const wsClient = createWSClient({
+  url: "wss://" + import.meta.env.VITE_API_URL + "/graphql",
+});
+
+export const graphqlClient = createClient({
+  url: "https://" + import.meta.env.VITE_API_URL + "/graphql",
+  fetchOptions: {
+    credentials: "same-origin",
+  },
+  exchanges: [
+    devtoolsExchange,
+    cache,
+    fetchExchange,
+    subscriptionExchange({
+      forwardSubscription(request) {
+        const input = { ...request, query: request.query || "" };
+        return {
+          subscribe(sink) {
+            const unsubscribe = wsClient.subscribe(input, sink);
+
+            wsClient.on("error", (error) => {
+              console.error("The subscription errored:", error);
+            });
+
+            return { unsubscribe };
+          },
+        };
+      },
+    }),
+  ],
+});
